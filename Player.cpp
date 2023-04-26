b251e24d6602577493c80ca5b9f56cff13de0029
大陸 永井 <k022g0053@m.neec.ac.jp>
2023-04-26 17:13:39 +0900

    弾の発射を実装

--- Player.cpp
+++ Player.cpp
@@ -1,11 +1,13 @@
 #include "Player.h"
 
-#include "Vector3.h"
-#include "Matrix4x4.h"
-#include <cassert>
 #include "ImGuiManager.h"
+#include "Matrix4x4.h"
+#include "Vector3.h"
+#include <cassert>
 
 #include "Mymath.h"
+
+#include "PlayerBullet.h"
 
 void Player::Initialze(Model* model, uint32_t textureHandle) {
 	// NULL ポインタチェック
@@ -21,6 +23,9 @@ void Player::Initialze(Model* model, uint32_t textureHandle) {
 }
 
 void Player::Update() {
+
+	// 旋回
+	Rotate();
 
 	// 移動ベクトルの設定
 	Vector3 move = {0, 0, 0};
@@ -40,6 +45,7 @@ void Player::Update() {
 	} else if (input_->PushKey(DIK_DOWN)) {
 		move.y -= kCharacterSpeed;
 	}
+	
 
 	// 座標加算(ベクトルの加算)
 	worldTransform_.translation_ = Mymath::Add(worldTransform_.translation_, move);
@@ -54,12 +60,18 @@ void Player::Update() {
 	worldTransform_.translation_.y = max(worldTransform_.translation_.y, -kMoveLimitY);
 	worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
 
-	worldTransform_.matWorld_ = Mymath::MakeAffineMatrix4x4(
-	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+	// 行列を定数バッファに転送
+	worldTransform_.UpdateMatrix();
+
+	// 攻撃
+	Attack();
+
+	// 弾更新
+	if (bullet_) {
+		bullet_->Update();
+	}
 
 
-	// 行列を定数バッファに転送	
-	worldTransform_.TransferMatrix();
 	ImGui::Begin("Player");
 	float* translate[3] = {
 	    &worldTransform_.translation_.x, &worldTransform_.translation_.y,
@@ -71,5 +83,34 @@ void Player::Update() {
 void Player::Draw(const ViewProjection& viewProjection) {
 	// 3D モデルを描画
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+	// 弾描画
+	if (bullet_) {
+		bullet_->Draw(viewProjection);
+	}
+}
 
+// プライベート関数
+
+void Player::Rotate() {
+	// 回転速さ[ラジアン/frame]
+	const float kRotSpeed = 0.02f;
+
+	// 押した方向でベクトルを要求
+	if (input_->PushKey(DIK_A)) {
+		worldTransform_.rotation_.y -= kRotSpeed;
+	} else if (input_->PushKey(DIK_D)) {
+		worldTransform_.rotation_.y += kRotSpeed;
+	}
+}
+
+void Player::Attack() { 
+
+	if (input_->TriggerKey(DIK_SPACE)) {
+		// 弾を生成し、初期化
+		PlayerBullet* newBullet = new PlayerBullet();
+		newBullet->Initialize(model_, worldTransform_.translation_);
+
+		// 弾を登録する
+		bullet_ = newBullet;
+	}
 }
